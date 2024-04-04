fc154355cf2575ccd46e8a7d1c6ea533bd89e517
anxdpanic <anxdpanic@@users.noreply.github.com>
2024-04-04 19:01:59 -0400

    add CSRF_TRUSTED_ORIGINS to env

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -3,6 +3,7 @@ from .base import *
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # Database PostgreSQL
 DATABASES = {
--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -5,6 +5,7 @@ DJANGO_ROOT = '/home/app/'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # Database PostgreSQL
 DATABASES = {
